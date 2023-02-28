3a8bca4d0740e82894a1d772f9cdad68d9ac168c
Laur <laurcastro.l.c@gmail.com>
2023-02-28 18:03:37 -0500

    First Commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 6 */ 
+
 let searchForm = document.getElementById("searchForm");
 searchForm.addEventListener("submit", onSubmit);
 
@@ -9,92 +9,90 @@ let buttonsArray = localStorage.getItem("buttons");
 
 if(buttonsArray != null)
 {
-        buttonsArray = buttonsArray.split(",");
+    buttonsArray = buttonsArray.split(",");
 }
-
-
+else
+{
+    buttonsArray = [];
+}
 
 reloadButtons();
 
 function onSubmit(event)
 {
-	let searchTerm = document.getElementById("gifText").value;
-	buttonsArray.push(searchTerm);
-	localStorage.setItem("buttons", buttonsArray);
-	gifSearch(searchTerm);
-	addButton(searchTerm);
+    let searchTerm = document.getElementById("gifText").value;
+    buttonsArray.push(searchTerm);
+    localStorage.setItem("buttons", buttonsArray);
+    gifSearch(searchTerm);
+    addButton(searchTerm);
 
-	event.preventDefault();
-
-	
+    event.preventDefault();
 }
-
 
 function onClick(event)
 {
-	if(event.target.matches("button") == true)
-	{
-		gifSearch(event.target.textContent);
-	}
+    if(event.target.matches("button") == true)
+    {
+        gifSearch(event.target.textContent);
+    }
 }
 
-{
 async function gifSearch(name)
+{
+    let request = await fetch(`https://api.giphy.com/v1/gifs/search?q=${name}&key=BynLYG2YwQIWMT9a6l53N2ux2y276EJ7&limit=12`);
+    let requestJson = await request.json();
 
-	
-	let request = await fetch("http://api.giphy.com/v1/gifs/search?q=&api_key=BynLYG2YwQIWMT9a6l53N2ux2y276EJ7&limit=10");
-	let requestJson = await request.json();
-
-	displayGifs(requestJson);
+    displayGifs(requestJson);
 }
 
-function displayGifs(data)
+function displayGifs(jsonResult)
 {
-	let gifsDiv = document.getElementById("gifsDiv");
-	gifsDiv.innerHTML = "";
+    let gifsDiv = document.getElementById("gifsDiv");
+    gifsDiv.innerHTML = "";
 
-	for(i = 0; i < 3;i++)
-	{
-		let row = document.createElement("div");
-		row.className = "row";
+    for(i = 0; i < 3;i++)
+    {
+        let row = document.createElement("div");
+        row.className = "row";
 
-		for(j = 0; j < 4; j++)
-		{
-			let image = document.createElement("img");
-			image.src = data.results[i * 4 + j].media_formats.tinygif.url;
-			image.alt = "Image not found";
-			image.className = "p-3";
+        for(j = 0; j < 4; j++)
+        {
+            let image = document.createElement("img");
+            image.src = jsonResult.data[i * 4 + j].images.original.url;
+            image.alt = "Image not found";
+            image.width = "200";
+            image.className = "p-3";
 
-			let imageDiv = document.createElement("div");
-			imageDiv.className = "col";
-			imageDiv.appendChild(image);
+            let imageDiv = document.createElement("div");
+            imageDiv.className = "col";
+            imageDiv.appendChild(image);
 
-			row.appendChild(imageDiv);
-		}
+            row.appendChild(imageDiv);
+        }
 
-		gifsDiv.appendChild(row);
-	}
+        gifsDiv.appendChild(row);
+    }
 }
 
 function addButton(name)
 {
-	let newButton = document.createElement("button");
-	newButton.type = "button";
-	newButton.textContent = name;
-	newButton.className = "btn btn-secondary";
-	buttonsDiv.appendChild(newButton);
+    let newButton = document.createElement("button");
+    newButton.type = "button";
+    newButton.textContent = name;
+    newButton.className = "btn btn-secondary";
+    buttonsDiv.appendChild(newButton);
 }
 
 function reloadButtons()
 {
-	if(localStorage.getItem("buttons") != null)
-	{
-		let oldButtons = localStorage.getItem("buttons").split(",");
+    if(localStorage.getItem("buttons") != null)
+    {
+        let oldButtons = localStorage.getItem("buttons").split(",");
 
-		for(i = 0; i < oldButtons.length; i++)
-		{
-			addButton(oldButtons[i]);
-		}
-	}
+        for(i = 0; i < oldButtons.length; i++)
+        {
+            addButton(oldButtons[i]);
+        }
+    }
 }
 
